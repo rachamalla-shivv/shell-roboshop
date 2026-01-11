05ea057f95cfa368cbd5149960dbf787fb50db16
shivv <rachamallashivashankargoud0@gmail.com>
2026-01-11 13:43:27 +0530

    instances

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ SG_ID="sg-043e4456d364c0038"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     #to get private ip
     if [ $instance != 'frontend' ]; then
