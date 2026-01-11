6a350960c07c4fd1e442429f7fb662f30d8844fe
shivv <rachamallashivashankargoud0@gmail.com>
2026-01-11 14:55:28 +0530

    instances

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-043e4456d364c0038"
+ZONE_ID="Z01771181AKUPZX8DM2VP" # hosted zone DNS R53
+DOMAIN_NAME="shivv-aws.fun"
 
 for instance in $@
 do
@@ -10,9 +12,31 @@ do
     #to get private ip
     if [ $instance != 'frontend' ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_ID="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_ID="$DOMAIN_NAME"
     fi
 
     echo "$instance : $IP"
+
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Updating record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+  '
+
 done
