02cfb48a1fbf89996f636d8391f69c17bb49128b
shivv <rachamallashivashankargoud0@gmail.com>
2026-01-21 17:29:35 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -43,7 +43,7 @@ VALIDATE $? "enable nodejs:20"
 dnf install nodejs -y &>>$LOGS_FILE
 VALIDATE $? "installing nodejs"
 
-id roboshop 
+id roboshop &>>$LOGS_FILE
 if [ $? -ne 0 ]; then 
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
 else
