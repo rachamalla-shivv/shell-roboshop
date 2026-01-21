8dbe645e44e974f52e5c12021331cb2c334e9b08
shivv <rachamallashivashankargoud0@gmail.com>
2026-01-21 16:43:25 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
  
+ set -euo pipefail
+
+ trap 'echo "there is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
  
 USERID=$( id -u )
 
@@ -17,6 +20,8 @@ N="\e[0m"
 
 LOG_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.shivv-aws.fun
 LOGS_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOG_FOLDER
@@ -38,3 +43,39 @@ VALIDATE $? "enable nodejs:20"
 dnf install nodejs -y
 VALIDATE $? "installing nodejs"
 
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then 
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+else
+    echo -e "user already exists ....$Y SKIPPING $N"
+fi
+
+mkdir -p /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOGS_FILE
+cd /app
+rm -rf /app/* 
+unzip /tmp/catalogue.zip
+
+npm install &>>$LOGS_FILE
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service 
+
+systemctl daemon-reload
+
+systemctl enable catalogue &>>$LOGS_FILE 
+
+echo -e "catalogue application setup .... $G SUCCESS $N"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+
+INDEX=$(mongosh mongodb.shivv-aws.fun --quiet --eval "db.getMongo().getDBNames().indexof('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOGS_FILE
+else
+    echo -e "catalogue products already loaded.... $Y skipping $N"
+fi
+
+systemctl restart catalogue 
+
+echo -e "Loading products and restarting catalogue .. $G success $N"
