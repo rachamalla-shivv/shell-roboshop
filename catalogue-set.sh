14c36096acbb5c60482419e8c7a3a37455f59c9a
shivv <rachamallashivashankargoud0@gmail.com>
2026-01-21 17:58:04 +0530

    catalogue

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -49,7 +49,7 @@ systemctl enable catalogue &>>$LOG_FILE
 echo -e "Catalogue application setup ... $G SUCCESS $N"
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mongoshsfds -y &>>$LOG_FILE
+dnf install mongodb-mongosh -y &>>$LOG_FILE
 
 INDEX=$(mongosh mongodb.shivv-aws.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
