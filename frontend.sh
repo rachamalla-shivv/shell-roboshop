da74048225a053de1b8b2111c02d6c1f93803362
shivv <rachamallashivashankargoud0@gmail.com>
2026-01-23 19:32:26 +0530

    catalogue

--- frontend.sh
+++ frontend.sh
@@ -30,9 +30,9 @@ VALIDATE() {
     fi
 }
 
-dnf module disable nginx -y &>>$LOG_FILE
-dnf module enable nginx:1.24 -y &>>$LOG_FILE
-dnf install nginx -y &>>$LOG_FILE
+dnf module disable nginx -y    &>>$LOG_FILE
+dnf module enable nginx:1.24 -y  &>>$LOG_FILE
+dnf install nginx -y     &>>$LOG_FILE
 
 VALIDATE $? " installing nginx "
 
