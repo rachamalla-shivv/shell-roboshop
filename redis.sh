327f79937de52cb7380b7195b789f7b26d89bce2
shivv <rachamallashivashankargoud0@gmail.com>
2026-01-23 20:00:40 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -17,17 +17,19 @@ N="\e[0m"
 
 LOG_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOGS_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
 dnf module disable redis -y &>>$LOG_FILE
-dnf module enable redis:7 -y &>>$LOG_FIL
+dnf module enable redis:7 -y &>>$LOG_FILE
 
-dnf install redis -y &>>$LOG_FIL
+dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "installing redis"
 
 sed -i -e 's/127.0.0.0/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redid/redis.conf
 VALIDATE $? "changing config"
 
-systemctl enable redis 
+systemctl enable redis &>>$LOG_FILE
 systemctl start redis 
 VALIDATE $? "start redis"
+
+systemctl status redis
