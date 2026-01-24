070d903703a71d439a774395898a8c024f56d47c
shivv <rachamallashivashankargoud0@gmail.com>
2026-01-24 11:30:13 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -1,48 +1,47 @@
 #!/bin/bash
- 
-#!/bin/bash
- 
- 
-USERID=$( id -u )
 
-echo "Script started executed at: $(date) "
-if [ $USERID -ne 0 ]; then
-    echo "error:: plese run this script with root privelege"
-    exit 1
-fi
-
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
-LOG_FOLDER="/var/log/shell-roboshop"
-SCRIPT_DIR=$PWD
+LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+START_TIME=$(date +%s)
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
-mkdir -p $LOG_FOLDER
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
 
-VALIDATE() {
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e " $2 is..$R FAILURE $N"
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
     else
-        echo -e " $2 is ..$G SUCCESS $N"
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
 dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling Default Redis"
 dnf module enable redis:7 -y &>>$LOG_FILE
-
-dnf install redis -y &>>$LOG_FILE
-VALIDATE $? "installing redis"
+VALIDATE $? "Enabling Redis 7"
+dnf install redis -y  &>>$LOG_FILE
+VALIDATE $? "Installing Redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "changing config"
+VALIDATE $? "Allowing Remote connections to Redis"
 
 systemctl enable redis &>>$LOG_FILE
-systemctl start redis 
-VALIDATE $? "start redis"
+VALIDATE $? "Enabling Redis"
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Starting Redis"
 
-systemctl status redis
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
