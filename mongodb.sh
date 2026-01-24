338981b81ddd22e877b7f48618a8836cd777234a
shivv <rachamallashivashankargoud0@gmail.com>
2026-01-24 14:46:58 +0530

    shippig

--- mongodb.sh
+++ mongodb.sh
@@ -1,48 +1,47 @@
+
 #!/bin/bash
- 
- 
-USERID=$( id -u )
 
-echo "Script started executed at: $(date) "
-if [ $USERID -ne 0 ]; then
-    echo "error:: plese run this script with root privelege"
-    exit 1
-fi
-
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
-LOG_FOLDER="/var/log/shell-roboshop"
+LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOGS_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
-mkdir -p $LOG_FOLDER
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
-VALIDATE() {
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e " $2 is..$R FAILURE $N"
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
     else
-        echo -e " $2 is ..$G SUCCESS $N"
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "adding mongo repo"
+VALIDATE $? "Adding Mongo repo"
 
-dnf install mongodb-org -y &>>$LOGS_FILE
-VALIDATE $? "installing mongo"
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB"
 
-systemctl enable mongod
-VALIDATE $? "enable mongodb"
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enable MongoDB"
 
-systemctl start mongod
-VALIDATE $? "starting mongodb"
+systemctl start mongod 
+VALIDATE $? "Start MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-VALIDATE $? "changing mongod.config"
+VALIDATE $? "Allowing remote connections to MongoDB"
 
 systemctl restart mongod
-VALIDATE $? "RESTART MONGODB"
+VALIDATE $? "Restarted MongoDB"
