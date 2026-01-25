d4ae2cac27a7e754e6970dce1af3a03cbdcbbbcb
shivv <rachamallashivashankargoud0@gmail.com>
2026-01-25 12:20:25 +0530

    shippig

--- roboshop.sh
+++ roboshop.sh
@@ -1,3 +1,5 @@
+
+
 #!/bin/bash
 
 AMI_ID="ami-0220d79f3f480ecf5" #OS
@@ -5,21 +7,20 @@ SG_ID="sg-07dcbad23f5972a86"
 ZONE_ID="Z01771181AKUPZX8DM2VP" # hosted zone DNS R53 Z01771181AKUPZX8DM2VP
 DOMAIN_NAME="shivv-aws.fun"
 
-for instance in $@
+for instance in $@ # mongodb redis mysql
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-    #to get private ip
-    if [ $instance != 'frontend' ]; then
+    # Get Private IP
+    if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        RECORD_ID="$instance.$DOMAIN_NAME"
-        
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        RECORD_ID="$DOMAIN_NAME"
+        RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
     fi
 
-    echo "$instance : $IP"
+    echo "$instance: $IP"
 
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
@@ -29,7 +30,7 @@ do
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$RECORD_ID'"
+            "Name"              : "'$RECORD_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
@@ -39,5 +40,4 @@ do
         }]
     }
     '
-
 done
