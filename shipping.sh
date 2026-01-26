adca73b4e3f285922cdaf284160da2c9898bb811
shivv <rachamallashivashankargoud0@gmail.com>
2026-01-26 11:52:36 +0530

    shippig

--- shipping.sh
+++ shipping.sh
@@ -12,7 +12,7 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.daws86s.fun
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-MYSQL_HOST=mysql.daws86s.fun
+MYSQL_HOST=mysql.shivv-aws.fun
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
